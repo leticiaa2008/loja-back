8cd038e9456bccb1a0776eed86030c6371fd4851
Letícia Cerial <leticia.cerial@aluno.senai.br>
2026-05-29 10:31:35 -0300

    Delete src directory

--- src/routes/clienteRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const clienteController = require('../controllers/clienteController');
-
-router.post('/cadastro', clienteController.cadastrarCliente);
-router.post('/login', clienteController.loginCliente);
-
-module.exports = router;
--- src/routes/pedidoRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const pedidoController = require('../controllers/pedidoController');
-
-router.post('/', pedidoController.criarPedido);
-router.get('/:clienteId', pedidoController.listarPedidosCliente);
-
-module.exports = router;
